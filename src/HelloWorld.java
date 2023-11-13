7f824faf65a18c6c222b6a359a3e776b1a50b619
Daniel Gibbs-Egan <150727770+DanielGibbs-Egan@users.noreply.github.com>
2023-11-13 13:17:17 -0800

    Update HelloWorld.java

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,6 +3,8 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// print "Hello World!" to console
 		System.out.println("Hello World!");
+		// print "Goodbye!" to console
+		System.out.println("Goodbye!");
 		
 	}
 }
