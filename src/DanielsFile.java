6ba85087be4e5fd210d4c625f50758465c568a81
makei <makei@CodableKevin.hitronhub.home>
2023-11-17 15:53:09 -0800

    Random Int Gen

--- src/DanielsFile.java
+++ src/DanielsFile.java
@@ -1,8 +1,18 @@
 
 public class DanielsFile {
+	
+	public static int randomInt(int low, int high) {
+		                                 // 5 - 1       
+		int random = (int)(Math.random()*(high-low)) + low;
+		
+		return random;
+		
+	}
 	public static void main(String[] args) {
 		// Hello World!
 		// Prints my Name!
 		System.out.println("Daniel Gibbs-Egan");
+		System.out.println(randomInt(0,10));
 	}
+
 }
